2909f8ab1a340bf43bebf6e36a58785605538989
Thiernoh <thiernohgradiagram@gmail.com>
2024-02-06 20:32:12 -0500

    project setup

---
+++ android/app/src/main/java/com/thiernollc/flutter/a_flutter_app_thierno/a_flutter_app_thierno/MainActivity.java
@@ -0,0 +1,6 @@
+package com.thiernollc.flutter.a_flutter_app_thierno.a_flutter_app_thierno;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
